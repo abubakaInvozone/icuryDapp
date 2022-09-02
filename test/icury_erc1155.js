dd0100d7323f4074b74a97f2b94b89cb30e2ede8
abubakar.waqas <abubakar.waqas@invozone.com>
2022-09-02 18:31:33 +0500

    test unit case

--- test/icury_erc1155.js
+++ test/icury_erc1155.js
@@ -12,3 +12,16 @@ describe("Token contract", function () {
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
 });
+
+describe("Token contract", function () {
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("IcuryErc1155");
+
+    const hardhatToken = await Token.deploy();
+
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  });
+});
